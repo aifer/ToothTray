d07426e85392332c1266b663522e1efbda23eb1a
JiangNan <lzty@vip.qq.com>
2026-09-04 21:11:27 +0800

    Implement custom headphones icon in ToothTrayUse a modern blue headphones tray icon
    Refactor the ToothTray application to create a custom headphones icon.

--- ToothTray/ToothTray.cpp
+++ ToothTray/ToothTray.cpp
@@ -13,17 +13,85 @@
 
 #define MAX_LOADSTRING 100
 
-// Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+HINSTANCE hInst;
+WCHAR szTitle[MAX_LOADSTRING];
+WCHAR szWindowClass[MAX_LOADSTRING];
 constexpr UINT WM_TRAYICON = WM_APP;
 
 BluetoothAudioDeviceEnumerator bluetoothAudioDeviceEmumerator;
 ToothTrayMenu trayMenu;
 TrayIcon trayIcon;
 
-// Forward declarations of functions included in this code module:
+static HICON CreateHeadphonesIcon()
+{
+    constexpr int iconSize = 32;
+
+    BITMAPV5HEADER bitmapInfo = {};
+    bitmapInfo.bV5Size = sizeof(bitmapInfo);
+    bitmapInfo.bV5Width = iconSize;
+    bitmapInfo.bV5Height = -iconSize;
+    bitmapInfo.bV5Planes = 1;
+    bitmapInfo.bV5BitCount = 32;
+    bitmapInfo.bV5Compression = BI_BITFIELDS;
+    bitmapInfo.bV5RedMask = 0x00ff0000;
+    bitmapInfo.bV5GreenMask = 0x0000ff00;
+    bitmapInfo.bV5BlueMask = 0x000000ff;
+    bitmapInfo.bV5AlphaMask = 0xff000000;
+
+    void* rawPixels = nullptr;
+    HDC screen = GetDC(nullptr);
+    HBITMAP colorBitmap = CreateDIBSection(screen, reinterpret_cast<BITMAPINFO*>(&bitmapInfo),
+        DIB_RGB_COLORS, &rawPixels, nullptr, 0);
+    ReleaseDC(nullptr, screen);
+
+    if (!colorBitmap)
+        return LoadIconW(nullptr, IDI_APPLICATION);
+
+    HDC canvas = CreateCompatibleDC(nullptr);
+    HGDIOBJ oldBitmap = SelectObject(canvas, colorBitmap);
+    const COLORREF iconColor = RGB(39, 148, 255);
+    HPEN pen = CreatePen(PS_SOLID, 3, iconColor);
+    HBRUSH brush = CreateSolidBrush(iconColor);
+    HGDIOBJ oldPen = SelectObject(canvas, pen);
+    HGDIOBJ oldBrush = SelectObject(canvas, brush);
+
+    SetBkMode(canvas, TRANSPARENT);
+    Arc(canvas, 5, 3, 27, 29, 5, 18, 27, 18);
+    RoundRect(canvas, 4, 16, 10, 28, 3, 3);
+    RoundRect(canvas, 22, 16, 28, 28, 3, 3);
+
+    SelectObject(canvas, oldBrush);
+    SelectObject(canvas, oldPen);
+    DeleteObject(brush);
+    DeleteObject(pen);
+    SelectObject(canvas, oldBitmap);
+    DeleteDC(canvas);
+
+    DWORD* pixels = static_cast<DWORD*>(rawPixels);
+    for (int i = 0; i < iconSize * iconSize; ++i) {
+        if ((pixels[i] & 0x00ffffff) != 0)
+            pixels[i] |= 0xff000000;
+    }
+
+    HBITMAP maskBitmap = CreateBitmap(iconSize, iconSize, 1, 1, nullptr);
+    HDC maskCanvas = CreateCompatibleDC(nullptr);
+    HGDIOBJ oldMask = SelectObject(maskCanvas, maskBitmap);
+    PatBlt(maskCanvas, 0, 0, iconSize, iconSize, BLACKNESS);
+    SelectObject(maskCanvas, oldMask);
+    DeleteDC(maskCanvas);
+
+    ICONINFO iconInfo = {};
+    iconInfo.fIcon = TRUE;
+    iconInfo.hbmMask = maskBitmap;
+    iconInfo.hbmColor = colorBitmap;
+    HICON icon = CreateIconIndirect(&iconInfo);
+
+    DeleteObject(maskBitmap);
+    DeleteObject(colorBitmap);
+
+    return icon ? icon : LoadIconW(nullptr, IDI_APPLICATION);
+}
+
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -37,71 +105,50 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
     winrt::init_apartment();
 
-    // Initialize global strings
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_TOOTHTRAY, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
+    if (!InitInstance(hInstance, nCmdShow))
     {
         return FALSE;
     }
 
     MSG msg;
-
-    // Main message loop:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    return (int) msg.wParam;
+    return (int)msg.wParam;
 }
 
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
 
     wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TOOTHTRAY));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TOOTHTRAY);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TOOTHTRAY));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_TOOTHTRAY);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
 }
 
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+   hInst = hInstance;
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
@@ -111,31 +158,13 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
       return FALSE;
    }
 
-   //bluetoothRadio = BluetoothRadio::FindFirst();
-   //bluetoothRadio.RegisterDeviceChange(hWnd);
-   //watcher = std::make_unique<BluetoothDeviceWatcher>();
-   //watcher->Start();
-
-   HICON hIcon = (HICON)LoadImageW(hInstance, MAKEINTRESOURCE(IDI_TOOTHTRAY), IMAGE_ICON, 0, 0, LR_DEFAULTSIZE);
+   HICON hIcon = CreateHeadphonesIcon();
    trayIcon.Initialize(hWnd, hIcon, 0, WM_TRAYICON, NULL);
 
-   //ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
-
    return TRUE;
 }
 
-
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE: Processes messages for the main window.
-//
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
@@ -143,7 +172,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_COMMAND:
         {
             int commandId = LOWORD(wParam);
-            // Parse the menu selections:
             switch (commandId)
             {
             case IDM_EXIT:
@@ -160,9 +188,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
-    //case WM_DEVICECHANGE:
-    //    BluetoothRadio::HandleDeviceChangeMessage(wParam, lParam);
-    //    break;
     default:
         WORD event;
         if (trayIcon.HandleMessage(message, lParam, &event)) {
